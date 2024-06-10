ebb2d157ea92b3ca449d598fa0a78f6b37aae711
redsylx <fakhrialbari.red@gmail.com>
2024-06-10 10:25:52 +0700

    update entry script

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import Root from "./routes/root"
+import ErrorPage from "./routes/error-page";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+    errorElement: <ErrorPage/>
+  },
+  {
+    path: "/root",
+    element: <Root />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
